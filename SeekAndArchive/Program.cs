642be8d686c914a7498e11f67753504fc8f6dc05
Berta Vincze <berta.vincze@gmail.com>
2019-06-21 10:12:34 +0200

    Add searching functionality

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SeekAndArchive
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            string fileName = args[0];
+            string directoryName = args[1];
+            FoundFiles = new List<FileInfo>();
+
+            DirectoryInfo rootDir = new DirectoryInfo(directoryName);
+
+            if (!rootDir.Exists)
+            {
+                Console.WriteLine("The specified directory does not exist.");
+                return;
+            }
+
+            RecursiveSearch(FoundFiles, fileName, rootDir);
+            Console.WriteLine("Found {0} files.", FoundFiles.Count);
+
+            foreach (FileInfo file in FoundFiles)
+            {
+                Console.WriteLine("{0}", file.FullName);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static List<FileInfo> FoundFiles { get; set; }
+
+        static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
+        {
+            foreach (FileInfo file in currentDirectory.GetFiles())
+            {
+                if (file.Name == fileName) foundFiles.Add(file);
+            }
+
+            foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, fileName, dir);
+            }
         }
     }
 }
