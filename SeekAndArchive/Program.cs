7069491c26eb70bacc0106022a67b06a38212742
Berta Vincze <berta.vincze@gmail.com>
2019-06-21 10:21:38 +0200

    Add FileSystemWatchers for found files to alert the user on file content change

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -11,6 +11,7 @@ namespace SeekAndArchive
             string fileName = args[0];
             string directoryName = args[1];
             FoundFiles = new List<FileInfo>();
+            Watchers = new List<FileSystemWatcher>();
 
             DirectoryInfo rootDir = new DirectoryInfo(directoryName);
 
@@ -28,10 +29,19 @@ namespace SeekAndArchive
                 Console.WriteLine("{0}", file.FullName);
             }
 
+            foreach (FileInfo fil in FoundFiles)
+            {
+                FileSystemWatcher newWatcher = new FileSystemWatcher(fil.DirectoryName, fil.Name);
+                newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
+                newWatcher.EnableRaisingEvents = true;
+                Watchers.Add(newWatcher);
+            }
+
             Console.ReadKey();
         }
 
         public static List<FileInfo> FoundFiles { get; set; }
+        public static List<FileSystemWatcher> Watchers { get; set; }
 
         static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
         {
@@ -43,6 +53,14 @@ namespace SeekAndArchive
             foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
             {
                 RecursiveSearch(foundFiles, fileName, dir);
+            }
+        }
+
+        static void WatcherChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                Console.WriteLine("{0} has been changed!", e.FullPath);
             }
         }
     }
